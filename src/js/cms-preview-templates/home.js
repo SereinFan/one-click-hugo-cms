1faf4e8180698abf72d069b6c465338aba9712e1
SereinFan <127286873+SereinFan@users.noreply.github.com>
2025-03-28 16:04:26 +0800

    Update home.js

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -46,7 +46,7 @@ export default class HomePreview extends React.Component {
               <div className="w-60-l mb0">
                 {widgetFor("values")}
               </div>
-              <!--p{entry.getIn(["data", "values", "text"])}/p-->
+              
             </div>
 
             <div className="w-60-l ph2-l">
