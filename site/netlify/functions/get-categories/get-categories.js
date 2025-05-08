0d13da4b8a0d7bf72d57263a72f227e49c5496c8
SereinFan <127286873+SereinFan@users.noreply.github.com>
2025-05-08 10:28:26 +0000

    Update get-categories.js

--- site/netlify/functions/get-categories/get-categories.js
+++ site/netlify/functions/get-categories/get-categories.js
@@ -1,26 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const matter = require('gray-matter'); // 导入 gray-matter
 
 exports.handler = async function(event, context) {
   try {
-    // 修改路径为相对于函数文件的路径
     const categoriesPath = path.resolve(__dirname, '../../../content/categories/_index.md');
-    console.log('Resolved file path:', categoriesPath); // 打印解析后的路径
+    console.log('Resolved file path:', categoriesPath);
 
-    // 读取文件内容
-    const fileContent = fs.readFileSync(categoriesPath, 'utf8'); // 添加读取文件内容的操作
-    console.log('File content:', fileContent);  // 打印文件内容，用于调试
+    if (!fs.existsSync(categoriesPath)) {
+      throw new Error(`File not found at path: ${categoriesPath}`);
+    }
 
-    // 使用正则解析文件内容
-    const categories = fileContent.match(/name: "(.+)"/g)
-      .map(match => match.replace(/name: "/, '').replace(/"/, ''));
+    const fileContent = fs.readFileSync(categoriesPath, 'utf8');
+    console.log('File content:', fileContent);
+
+    // 使用 gray-matter 解析 Front Matter
+    const { data } = matter(fileContent);
+    console.log('Parsed data:', data);
+
+    const categories = data.categories_list ? data.categories_list.map(category => category.name) : [];
+    console.log('Categories:', categories);
 
     return {
       statusCode: 200,
       body: JSON.stringify(categories),
     };
   } catch (err) {
-    console.error('Error reading categories file:', err);
+    console.error('Error reading or parsing categories file:', err);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: err.message }),
