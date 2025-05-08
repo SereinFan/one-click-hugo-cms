288c06323d39aec081717ac1d373a372e3003190
SereinFan <127286873+SereinFan@users.noreply.github.com>
2025-05-08 06:56:04 +0000

    Update category-widget.js

--- site/static/admin/js/category-widget.js
+++ site/static/admin/js/category-widget.js
@@ -1,31 +1,36 @@
-// 修正后的 category-widget.js
-   import CMS from 'netlify-cms-app';
-   import { createClass, h } from 'netlify-cms-ui-default';
+import { CMS } from 'netlify-cms-app';
 
-   const CategoriesControl = createClass({
-     getInitialState: function() {
-       return { categories: [] };
-     },
+const CategoriesControl = createClass({
+  getInitialState: function() {
+    return {
+      categories: []
+    };
+  },
 
-     componentDidMount: function() {
-       fetch('/.netlify/functions/get-categories')
-         .then(response => response.json())
-         .then(categories => this.setState({ categories }))
-         .catch(console.error);
-     },
+  componentDidMount: function() {
+    fetch('/.netlify/functions/get-categories')
+      .then(response => response.json())
+      .then(categories => {
+        console.log('Fetched categories:', categories); // 添加调试日志
+        this.setState({ categories });
+      })
+      .catch(error => console.error('Error fetching categories:', error));
+  },
 
-     render: function() {
-       const { value, onChange } = this.props;
-       return h('select', {
-           value: value || '',
-           onChange: (e) => onChange([...e.target.selectedOptions].map(o => o.value)),
-           multiple: true
-         },
-         this.state.categories.map(category =>
-           h('option', { value: category, key: category }, category)
-         )
-       );
-     }
-   });
+  render: function() {
+    const { value, onChange } = this.props;
+    const options = this.state.categories.map(category =>
+      h('option', { value: category }, category)
+    );
 
-   CMS.registerWidget('categories', CategoriesControl);
+    return h('select', {
+      value: value || '',
+      onChange: (e) => onChange(e.target.value),
+      multiple: true
+    }, options);
+  }
+});
+
+// 注册控件
+CMS.registerWidget('categories', CategoriesControl);
+console.log('Categories widget registered successfully.'); // 添加调试日志
