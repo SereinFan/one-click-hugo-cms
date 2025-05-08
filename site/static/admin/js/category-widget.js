fa48a5a28d2b7eee83f70460d76f88b52bc0e580
SereinFan <127286873+SereinFan@users.noreply.github.com>
2025-05-08 03:57:07 +0000

    Update category-widget.js

--- site/static/admin/js/category-widget.js
+++ site/static/admin/js/category-widget.js
@@ -1,31 +1,31 @@
-import { CMS } from 'netlify-cms-app';
+// 修正后的 category-widget.js
+   import CMS from 'netlify-cms-app';
+   import { createClass, h } from 'netlify-cms-ui-default';
 
-const CategoriesControl = createClass({
-  getInitialState: function() {
-    return {
-      categories: []
-    };
-  },
+   const CategoriesControl = createClass({
+     getInitialState: function() {
+       return { categories: [] };
+     },
 
-  componentDidMount: function() {
-    fetch('/.netlify/functions/get-categories')
-      .then(response => response.json())
-      .then(categories => this.setState({ categories }))
-      .catch(error => console.error('Error fetching categories:', error)); // 添加错误处理
-  },
+     componentDidMount: function() {
+       fetch('/.netlify/functions/get-categories')
+         .then(response => response.json())
+         .then(categories => this.setState({ categories }))
+         .catch(console.error);
+     },
 
-  render: function() {
-    const { value, onChange } = this.props;
-    return h('select', {
-      value: value || '',
-      onChange: (e) => onChange(e.target.value),
-      multiple: true
-    },
-      this.state.categories.map(category =>
-        h('option', { value: category }, category)
-      )
-    );
-  }
-});
+     render: function() {
+       const { value, onChange } = this.props;
+       return h('select', {
+           value: value || '',
+           onChange: (e) => onChange([...e.target.selectedOptions].map(o => o.value)),
+           multiple: true
+         },
+         this.state.categories.map(category =>
+           h('option', { value: category, key: category }, category)
+         )
+       );
+     }
+   });
 
-CMS.registerWidget('categories', CategoriesControl);
+   CMS.registerWidget('categories', CategoriesControl);
