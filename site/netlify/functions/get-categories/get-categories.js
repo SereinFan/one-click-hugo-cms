4b15f73b4e2a831b4f4ed7afec0f6158ad581d7f
SereinFan <127286873+SereinFan@users.noreply.github.com>
2025-05-08 09:49:32 +0000

    Update get-categories.js

--- site/netlify/functions/get-categories/get-categories.js
+++ site/netlify/functions/get-categories/get-categories.js
@@ -6,7 +6,9 @@ exports.handler = async function(event, context) {
   try {
     // 修改路径为相对于函数文件的路径
     const categoriesPath = path.resolve(__dirname, '../../../content/categories/_index.md');
+    const fileContent = fs.readFileSync(categoriesPath, 'utf8');
     console.log('Resolved file path:', categoriesPath); // 打印解析后的路径
+
 
     // 使用正则解析文件内容
     const categories = fileContent.match(/name: "(.+)"/g)
